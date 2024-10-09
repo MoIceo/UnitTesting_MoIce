ca28a3bfca43513d78b36c08cd86bc1581ba993d
MoIceo <146645071+MoIceo@users.noreply.github.com>
2024-10-09 11:42:05 +0300

    Added WeatherTests

---
+++ UnitTesting/WeatherTests.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+using TestingLib.Weather;
+
+namespace UnitTesting
+{
+    public class WeatherTests
+    {
+        private readonly Mock<IWeatherForecastSource>? mockWeatherForecastSource;
+
+        public WeatherTests()
+        {
+            mockWeatherForecastSource = new Mock<IWeatherForecastSource>();
+        }
+
+        [Fact]
+        public void GetWeatherForecast_ShouldReturnForecast()
+        {
+            var weatherForecast = new WeatherForecast { Date = DateTime.Today, TemperatureC = 666, Summary = "Всем [ОТРЕДАКТИРОВАННО]!" };
+
+            mockWeatherForecastSource?.Setup(repo => repo.GetForecast(DateTime.Today)).Returns(weatherForecast);
+
+            var service = new WeatherForecastService(mockWeatherForecastSource.Object);
+
+            var result = service.GetWeatherForecast(DateTime.Today);
+
+            Assert.NotNull(result);
+        }
+        
+    }
+}
